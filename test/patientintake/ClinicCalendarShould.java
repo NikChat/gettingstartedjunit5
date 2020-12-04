9fecd1ad576ce4c35387b9c1bc90c761744c776e
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2020-12-04 14:25:12 +0200

    Making Existing Code Testable

--- test/patientintake/ClinicCalendarShould.java
+++ test/patientintake/ClinicCalendarShould.java
@@ -5,21 +5,21 @@ import org.junit.jupiter.api.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("ClinicCalendar should")
 class ClinicCalendarShould {
 
    private ClinicCalendar calendar;
 
-   //@BeforeAll -> will run once before all test methods in the class
-   @BeforeEach // will run once before each @Test method
+   @BeforeEach
    void init() {
       calendar = new ClinicCalendar(LocalDate.of(2018, 8, 26));
    }
 
    @Test
+   @DisplayName("record a new appointment correctly")
    void allowEntryOfAnAppointment() {
       calendar.addAppointment("Jim", "Weaver", "avery",
          "09/01/2018 2:00 pm");
@@ -28,41 +28,61 @@ class ClinicCalendarShould {
       assertEquals(1, appointments.size());
       PatientAppointment enteredAppt = appointments.get(0);
 
-//      assertEquals("Jim", enteredAppt.getPatientFirstName());
-//      assertEquals("Weaver", enteredAppt.getPatientLastName());
-//      assertSame(Doctor.avery, enteredAppt.getDoctor()); //exactly the same object in memory
-//      assertEquals("9/1/2018 02:00 PM",
-//         enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)));
-//      
       assertAll(
          () -> assertEquals("Jim", enteredAppt.getPatientFirstName()),
          () -> assertEquals("Weaver", enteredAppt.getPatientLastName()),
          () -> assertSame(Doctor.avery, enteredAppt.getDoctor()),
          () -> assertEquals("9/1/2018 02:00 PM",
-            enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)))
+            enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a")))
       );
    }
 
-   @Test
-   void returnTrueForHasAppointmentsIfThereAreAppointments() {
-      calendar.addAppointment("Jim", "Weaver", "avery",
-         "09/01/2018 2:00 pm");
-      assertTrue(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
+   @Nested
+   @DisplayName("indicate if there are appointments correctly")
+   class HasAppointments {
+
+      @Test
+      @DisplayName("when there are appointments")
+      void returnTrueForHasAppointmentsIfThereAreAppointments() {
+         calendar.addAppointment("Jim", "Weaver", "avery",
+            "09/01/2018 2:00 pm");
+         assertTrue(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
+      }
+
+      @Test
+      @DisplayName("when there are no appointments")
+      void returnFalseForHasAppointmentsIfThereAreNoAppointments() {
+         assertFalse(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
+      }
    }
 
-   @Test
-   void returnFalseForHasAppointmentsIfThereAreNoAppointments() {
-      assertFalse(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
+   @Nested
+   @DisplayName("return appointments correctly")
+   class AppointmentsForDay {
+
+      @Test
+      @DisplayName("for today")
+      void returnCurrentDaysAppointments() {
+         calendar.addAppointment("Jim", "Weaver", "avery",
+            "08/26/2018 2:00 pm");
+         calendar.addAppointment("Jim", "Weaver", "avery",
+            "08/26/2018 3:00 pm");
+         calendar.addAppointment("Jim", "Weaver", "avery",
+            "09/01/2018 2:00 pm");
+         assertEquals(2, calendar.getTodayAppointments().size());
+      }
+
+      @Test
+      @DisplayName("for tomorrow")
+      void returnTommorowsAppointments() {
+         calendar.addAppointment("Jim", "Weaver", "avery",
+            "08/27/2018 2:00 pm");
+         calendar.addAppointment("Jim", "Weaver", "avery",
+            "08/27/2018 2:00 pm");
+         calendar.addAppointment("Jim", "Weaver", "avery",
+            "08/26/2018 3:00 pm");
+         assertEquals(2, calendar.getTomorrowAppointments().size());
+      }
    }
 
-   @Test
-   void returnCurrentDaysAppointments() {
-      calendar.addAppointment("Jim", "Weaver", "avery",
-         "08/26/2018 2:00 pm");
-      calendar.addAppointment("Jim", "Weaver", "avery",
-         "08/26/2018 3:00 pm");
-      calendar.addAppointment("Jim", "Weaver", "avery",
-         "09/01/2018 2:00 pm");
-      assertEquals(2, calendar.getTodayAppointments().size());
-   }
 }
